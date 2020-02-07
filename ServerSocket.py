8f0ba838a9c5f63e1c04eebbb4b90b15d1b1573e
ddurbin1 <ddurbin1@live.maryville.edu>
2020-02-07 14:23:23 -0600

    Final Update
    Addition of Client two and the final code for the project

--- ServerSocket.py
+++ ServerSocket.py
@@ -5,9 +5,11 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind((socket.gethostname(), 9500))
 serv.listen(5)
 
-
 while True:
     conn, addr = serv.accept()
     msg = conn.recv(1024)
     print(msg.decode("utf-8"))
-    conn.send(bytes("Hi", "utf-8"))
+    if (msg == b"Hello"):
+        conn.send(bytes("Hi", "utf-8"))
+    else:
+        conn.send(bytes("Goodbye", "utf-8"))
